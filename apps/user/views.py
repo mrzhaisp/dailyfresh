7b79b69182a1804a57840849c809e6e552dd94ca
mr_zhai_sp <mr_zhai_sp@126.com>
2020-01-17 14:45:26 +0800

    添加token加密

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,65 +1,153 @@
 from django.shortcuts import render,redirect
 from django.core.urlresolvers import reverse
 import re
+from django.views.generic import View
 from user.models import User
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
 # Create your views here.
 
 
-# /user/register
-def register(request):
+# # /user/register
+# def register(request):
+#     '''注册'''
+#     if request.method=='GET':
+#         return render(request,'register.html')
+#     else:
+#         # 接收数据
+#         username = request.POST.get('user_name')
+#         password = request.POST.get('pwd')
+#         email = request.POST.get('email')
+#         allow = request.POST.get('allow')
+#         # 数据校验
+#         if not all([username, password, email]):
+#             # 数据不完整
+#             return render(request, 'register.html', {'errmsg': '数据不完整'})
+#
+#         if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+#             return render(request, 'register.html', {'errmsg': '邮箱不合法'})
+#
+#         if allow != 'on':
+#             return render(request, 'register.html', {'errmsg': '未勾选协议'})
+#
+#         # 用户名是否重复
+#         try:
+#             user = User.objects.get(username=username)
+#         except User.DoesNotExist:
+#             # 用户名不存在
+#             user = None
+#
+#         if user:
+#             # 存在
+#             return render(request, 'register.html', {'errmsg': '用户名已存在'})
+#
+#         # 处理
+#         # user = User()
+#         # user.username=username
+#         # user.password=password
+#         # user.save()
+#
+#         # 内置方法 进行用户注册
+#         user = User.objects.create_user(username, email, password)
+#         user.is_active = 0
+#         # 为0 则不激活
+#         user.save()
+#         # 应答 跳转到首页  反响解析
+#         return redirect(reverse('goods:index'))
+
+# def register_handle(request):
+#     '''进行注册的处理'''
+#     #接收数据
+#     username = request.POST.get('user_name')
+#     password = request.POST.get('pwd')
+#     email = request.POST.get('email')
+#     allow = request.POST.get('allow')
+#     #数据校验
+#     if not all([username,password,email]):
+#         #数据不完整
+#         return render(request,'register.html',{'errmsg':'数据不完整'})
+#
+#     if not  re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$',email):
+#         return render(request, 'register.html', {'errmsg': '邮箱不合法'})
+#
+#     if allow != 'on':
+#         return render(request, 'register.html', {'errmsg': '未勾选协议'})
+#
+#     #用户名是否重复
+#     try:
+#         user= User.objects.get(username=username)
+#     except User.DoesNotExist:
+#         #用户名不存在
+#         user = None
+#
+#     if user:
+#         #存在
+#         return render(request,'register.html',{'errmsg':'用户名已存在'})
+#
+#     # 处理
+#     # user = User()
+#     # user.username=username
+#     # user.password=password
+#     # user.save()
+#
+#     #内置方法 进行用户注册
+#     user = User.objects.create_user(username,email,password)
+#     user.is_active=0
+#     #为0 则不激活
+#     user.save()
+#     #应答 跳转到首页  反响解析
+#     # return redirect(reverse('goods:index'))
+
+class RegisterView(View):
     '''注册'''
-    return render(request,'register.html')
+    def get(self,request):
+        '''显示注册'''
+        return render(request,'register.html')
 
-def register_handle(request):
-    '''进行注册的处理'''
-    #接收数据
-    username = request.POST.get('user_name')
-    password = request.POST.get('pwd')
-    email = request.POST.get('email')
-    allow = request.POST.get('allow')
-    #数据校验
-    if not all([username,password,email]):
-        #数据不完整
-        return render(request,'register.html',{'errmsg':'数据不完整'})
+    def post(self,request):
+        '''注册处理'''
+        # 接收数据
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+        # 数据校验
+        if not all([username, password, email]):
+            # 数据不完整
+            return render(request, 'register.html', {'errmsg': '数据不完整'})
 
-    if not  re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$',email):
-        return render(request, 'register.html', {'errmsg': '邮箱不合法'})
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'errmsg': '邮箱不合法'})
 
-    if allow != 'on':
-        return render(request, 'register.html', {'errmsg': '未勾选协议'})
+        if allow != 'on':
+            return render(request, 'register.html', {'errmsg': '未勾选协议'})
 
-    #用户名是否重复
-    try:
-        user= User.objects.get(username=username)
-    except User.DoesNotExist:
-        #用户名不存在
-        user = None
+        # 用户名是否重复
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # 用户名不存在
+            user = None
 
-    if user:
-        #存在
-        return render(request,'register.html',{'errmsg':'用户名已存在'})
+        if user:
+            # 存在
+            return render(request, 'register.html', {'errmsg': '用户名已存在'})
+        # 处理
+        # user = User()
+        # user.username=username
+        # user.password=password
+        # user.save()
 
-    # 处理
-    # user = User()
-    # user.username=username
-    # user.password=password
-    # user.save()
+        # 内置方法 进行用户注册
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        # 为0 则不激活
+        user.save()
 
-    #内置方法 进行用户注册
-    user = User.objects.create_user(username,email,password)
-    user.is_active=0
-    #为0 则不激活
-    user.save()
-    #应答 跳转到首页  反响解析
-    return redirect(reverse('goods:index'))
+        #发送激活邮件  http://127.0.0.1:8000/user/active/3
 
-
-
-
-
-
-
-
+        # 应答 跳转到首页  反响解析
+        return redirect(reverse('goods:index'))
 
 
 
